fe542dd1048928b478ae640a1c45f5eaa58da78a
saadullah dar <saadullahdar.official@gmail.com>
2025-06-12 17:04:21 +0500

    for loop in Go

--- main.go
+++ main.go
@@ -19,19 +19,4 @@ func main() {
 	}
 	fmt.Println("#########################################")
 
-	fmt.Println()
-	fmt.Println("Work")
-	fmt.Println("1. " + workOnFiverrProfile)
-	fmt.Println("2. " + workOnFiverrPortfolio)
-
-	fmt.Println()
-
-	fmt.Println("Reward")
-	fmt.Println("1. " + reward)
-
-	fmt.Println()
-	fmt.Println("Watch")
-	fmt.Println("1. " + shortGoCourse)
-	fmt.Println("2. " + pythonCourse)
-
 }
